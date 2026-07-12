05846dc410646b342a36a2e8843cb3381d8429eb
ALEX-FEL <141037975+ALEX-FEL@users.noreply.github.com>
2026-07-12 18:57:48 +0100

    Updated places.ts

--- src/mocks/places.ts
+++ src/mocks/places.ts
@@ -11,8 +11,8 @@ export const currentLocation: Place = {
 export const places: Place[] = [
   { id: "p_airport", name: "Nsimalen Airport", subtitle: "Yaoundé", lat: 3.722, lng: 11.553 },
   // { id: "p_market", name: "Marché Central", subtitle: "Centre-ville", lat: 3.867, lng: 11.518 },
-  // { id: "p_bastos", name: "Bastos", subtitle: "Quartier diplomatique", lat: 3.891, lng: 11.514 },
-  // { id: "p_akwa", name: "Akwa", subtitle: "Quartier d'affaires", lat: 3.877, lng: 11.528 },
-  // { id: "p_omnisport", name: "Omnisport", subtitle: "Stade", lat: 3.877, lng: 11.503 },
-  // { id: "p_mvog", name: "Mvog-Mbi", subtitle: "Résidentiel", lat: 3.855, lng: 11.512 },
+  { id: "p_bastos", name: "Bastos", subtitle: "Quartier diplomatique", lat: 3.891, lng: 11.514 },
+  { id: "p_akwa", name: "Akwa", subtitle: "Quartier d'affaires", lat: 3.877, lng: 11.528 },
+  { id: "p_omnisport", name: "Omnisport", subtitle: "Stade", lat: 3.877, lng: 11.503 },
+  { id: "p_mvog", name: "Mvog-Mbi", subtitle: "Résidentiel", lat: 3.855, lng: 11.512 },
 ];
